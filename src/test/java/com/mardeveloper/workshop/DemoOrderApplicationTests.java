a4f92c0d40fb7e3073a5aea248b5115fbfa064f3
Marcos <guimaraes.mar@gmail.com>
2022-02-02 14:43:30 -0300

    Project created

---
+++ src/test/java/com/mardeveloper/workshop/DemoOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mardeveloper.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
